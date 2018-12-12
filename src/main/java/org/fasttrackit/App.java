98470bcc8f3275c08b89e5a79b4f8647fed3fc4b
dianaioanadan <diana.piclisam@artsoft-consult.ro>
2018-12-12 21:15:25 +0200

    studied classes, variables and pimitive data types

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -8,6 +8,26 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //declare a variable and instantiating a Car object
+        //this is also a called initalizing a variable
+       Car firstCompetitor = new Car();
+       firstCompetitor.name = "Dacia";
+       firstCompetitor.colour = "red";
+       firstCompetitor.doorCount = 5;
+       firstCompetitor.mileage = 9.5;
+
+       // local variables are variables declared inside a method
+        // they don't receive default values
+       Engine firstEngine = new Engine();
+       firstEngine.manufacturer = "Renault";
+
+       // sout prints a message to the console
+        System.out.println(firstEngine.manufacturer);
+
+       firstCompetitor.engine = firstEngine;
+       firstCompetitor.engine.manufacturer = "BMW";
+
+        System.out.println(firstEngine.manufacturer);
+        System.out.println(firstCompetitor.engine.manufacturer);
     }
 }
