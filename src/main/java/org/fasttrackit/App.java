02581b42a05b7d74046acc084991d254c84b2703
dianaioanadan <diana.piclisam@artsoft-consult.ro>
2018-12-19 20:49:49 +0200

    studied constructors

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -8,23 +8,27 @@ public class App
 {
     public static void main( String[] args )
     {
+        // local variables are variables declared inside a method
+        // they don't receive default values
+        Engine firstEngine = new Engine();
+        firstEngine.manufacturer = "Renault";
+
         //declare a variable and instantiating a Car object
         //this is also a called initalizing a variable
-       Car firstCompetitor = new Car();
+       Car firstCompetitor = new Car(new Engine());
        firstCompetitor.name = "Dacia";
        firstCompetitor.colour = "red";
        firstCompetitor.doorCount = 5;
        firstCompetitor.mileage = 9.5;
-       // local variables are variables declared inside a method
-        // they don't receive default values
-       Engine firstEngine = new Engine();
-       firstEngine.manufacturer = "Renault";
+
 
        // sout prints a message to the console
         System.out.println(firstEngine.manufacturer);
 
-       firstCompetitor.engine = firstEngine;
        firstCompetitor.engine.manufacturer = "BMW";
+
+        Car secondCompetitor = new Car(new Engine());
+        secondCompetitor.name = "Golf";
 
         System.out.println(firstEngine.manufacturer);
         System.out.println(firstCompetitor.engine.manufacturer);
--- src/main/java/org/fasttrackit/Car.java
+++ src/main/java/org/fasttrackit/Car.java
@@ -6,4 +6,8 @@ public class Car extends AutoVehicle{
     // instance variables (declared as properties of a class)
     // they receive default values (null for objects, 0 for numbers, false for booleans)
     int doorCount;
+
+    public Car(Engine engine) {
+        super(engine);
+    }
 }
