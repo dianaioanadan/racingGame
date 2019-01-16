11d649cdb585af0ff4e97f07b5117f415a2a3a0a
dianaioanadan <diana.piclisam@artsoft-consult.ro>
2019-01-16 21:24:33 +0200

    studied lists

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -10,6 +10,10 @@ public class App
     {
 
         Game game = new Game();
+
+        game.addCompetitors(2);
+        game.displayCompetitors();
+
         game.addTrack(0,new Track("Highway",300));
         game.addTrack(1,new Track("Seaside",100));
 
